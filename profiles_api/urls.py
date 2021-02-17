4ba60f6c2bfc074e5f06c0b5ab7aea1a56059b68
Orkhan <orik.ws2@gmail.com>
2021-02-18 02:00:19 +0400

    Not working permission

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -7,9 +7,10 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewser', views.HelloViewSet, base_name='hello-viewset')
-
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hello-view/', views.HelloApiView.as_view()),
     path('', include(router.urls))
+    
 ]
